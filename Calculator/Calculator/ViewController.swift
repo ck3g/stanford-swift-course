a828b1d6e47f53b8c0e8e83d466b6e82faf15b1b
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-06-10 22:22:19 +0800

    Allow to append digits into display

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var display: UILabel!
 
+    var userInTheMiddleOfTypingANumber = false
+
+    @IBAction func appendDigit(sender: UIButton) {
+        var digit = sender.currentTitle!
+        if userInTheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userInTheMiddleOfTypingANumber = true
+        }
+    }
 }
 
