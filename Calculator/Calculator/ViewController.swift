e5a70a677eb117edecb70370b9376b4ed5c3958b
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2015-06-10 21:45:21 +0800

    Calculator initial commit

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Vitaly Tatarintsev on 6/10/15.
+//  Copyright (c) 2015 Vitaly Tatarintsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
